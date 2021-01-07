1ae316966fac4e8fca00f377c026ca5178435c74
genesisschaerrer <genesisschaerrer.dev@gmail.com>
2021-01-07 10:58:53 -0700

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,71 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import axios from "axios"
+import './styles.css';
+
+
+
+
+class App extends Component {
+  constructor(){
+    super()
+
+    this.state={
+      todo: "",
+      todos: [{title: "mock data", done: false}]
+    }
+  }
+
+  componentDidMount(){
+    axios
+      .get("https://gms-flask-todo-api.herokuapp.com/todos")
+      .then(res => {
+        this.setState({
+          todos: res.data
+        })
+      })
+      .catch(err => console.err(err))
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      todo: e.target.value,
+    })
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    console.log("submit")
+  }
+
+  renderTodos = () => {
+    return this.state.todos.map(todo => {
+      return (
+        <div>
+          <h1>{todo.title}</h1>
+        </div>
+      )
+    })
+  }
+
+  render(){
+    return(
+      <div>
+        <h1>Todo List</h1>
+        <form className="add-todo" onSubmit={this.handleSubmit}>
+          <input 
+            type="text"
+            placeholder="add todo item"
+            onChange={this.handleChange}
+            value={this.state.todo}
+          />
+          <button type="submit">Add</button>
+        </form>
+        {this.renderTodos()}
+      </div>
+    )
+  }
+}
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,7 +74,3 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
